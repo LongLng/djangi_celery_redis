3cdc21c0ae9fd42c356e58aa2e81d6ff8bfc566c
ngoclongcoder <ngoclong.coder@gmail.com>
2023-02-21 16:41:16 +0700

    fisrt commit

---
+++ django_celery/mainapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+from .tasks import test_func
+
+
+def test(request):
+    test_func.delay()
+    return HttpResponse("Done")
+
